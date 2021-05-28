01eb2185a07cc3b808d1d6320630c45d0b0583bf
= <=>
2021-05-28 22:39:15 +0700

    done

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -1,10 +1,9 @@
 <?php
-	class Pdf
+class Pdf
+{
+
+	function __construct()
 	{
-		
-		function __construct()
-		{
-			include_once APPPATH.'/third_party/fpdf.php';
-		}
+		include_once APPPATH . '/third_party/fpdf.php';
 	}
-?>
+}
